db23051ef5f9853cd346a6418dda7c638a0a6338
writersblockchain <seanmconrad94@gmail.com>
2023-01-06 20:14:28 -0500

    griptape

--- react-actilist-snip20/src/App.js
+++ react-actilist-snip20/src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import {
+  bootstrap,
+  getAddress,
+  onAccountAvailable,
+  getNativeCoinBalance,
+  coinConvert,
+} from "@stakeordie/griptape.js";
 
 function App() {
+  var [address, setAddress] = useState("");
+  var [coins, setCoins] = useState(undefined);
+  var [isConnected, setIsConnected] = useState(false);
+
+  useEffect(() => {
+    const removeOnAccountAvailable = onAccountAvailable(() => {
+      setIsConnected(true);
+      setAddress(getAddress());
+      getBalance();
+    });
+
+    return () => {
+      removeOnAccountAvailable();
+    };
+  }, []);
+
+  const getBalance = async () => {
+    var balance = await getNativeCoinBalance();
+    balance = coinConvert(balance, 6, "human");
+    setCoins(balance);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Hello, Griptape!</h1>
+      <p>Is connected? {isConnected ? "Yes" : "No"}</p>
+      <button
+        onClick={() => {
+          bootstrap();
+        }}
+        disabled={isConnected}
+      >
+        Bootstrap
+      </button>
+      <p>Your address is: {address}</p>
+      <p>Your balance is: {coins}</p>
+    </>
   );
 }
-
 export default App;
