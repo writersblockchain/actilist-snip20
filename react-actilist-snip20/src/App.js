a01336aa283b3bf50b75d0008770dd05b4b08306
writersblockchain <seanmconrad94@gmail.com>
2023-01-06 21:12:14 -0500

    remove griptape

--- react-actilist-snip20/src/App.js
+++ react-actilist-snip20/src/App.js
@@ -1,50 +1,8 @@
-import React, { useState, useEffect } from "react";
-import {
-  bootstrap,
-  getAddress,
-  onAccountAvailable,
-  getNativeCoinBalance,
-  coinConvert,
-} from "@stakeordie/griptape.js";
+import "./App.css";
+import { SecretNetworkClient, Wallet } from "secretjs";
 
 function App() {
-  var [address, setAddress] = useState("");
-  var [coins, setCoins] = useState(undefined);
-  var [isConnected, setIsConnected] = useState(false);
-
-  useEffect(() => {
-    const removeOnAccountAvailable = onAccountAvailable(() => {
-      setIsConnected(true);
-      setAddress(getAddress());
-      getBalance();
-    });
-
-    return () => {
-      removeOnAccountAvailable();
-    };
-  }, []);
-
-  const getBalance = async () => {
-    var balance = await getNativeCoinBalance();
-    balance = coinConvert(balance, 6, "human");
-    setCoins(balance);
-  };
-
-  return (
-    <>
-      <h1>Hello, Griptape!</h1>
-      <p>Is connected? {isConnected ? "Yes" : "No"}</p>
-      <button
-        onClick={() => {
-          bootstrap();
-        }}
-        disabled={isConnected}
-      >
-        Bootstrap
-      </button>
-      <p>Your address is: {address}</p>
-      <p>Your balance is: {coins}</p>
-    </>
-  );
+  return <div className="App">Swag</div>;
 }
+
 export default App;
